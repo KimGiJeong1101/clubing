f01631dbf1f8732c05ea314fe994e7898c386492
hykworld <151594104+hykworld@users.noreply.github.com>
2024-08-11 09:36:25 +0900

    Update HomeSearch.jsx
    ㅁ

--- frontend/src/pages/auth/RegisterPage/address/HomeSearch.jsx
+++ frontend/src/pages/auth/RegisterPage/address/HomeSearch.jsx
@@ -15,7 +15,7 @@ const HomeSearch = ({ setSelectedSido, setSelectedSigoon, setSelectedDong }) => 
 
   useEffect(() => {
     if (searchTerm) {
-      fetch(`/req/data?service=data&request=GetFeature&data=LT_C_ADEMD_INFO&key=286E5CAE-A8D1-3D02-AB4E-2DF927614303&domain=${port}&attrFilter=emd_kor_nm:like:${searchTerm}`)
+      fetch(`api/req/data?service=data&request=GetFeature&data=LT_C_ADEMD_INFO&key=286E5CAE-A8D1-3D02-AB4E-2DF927614303&domain=${port}&attrFilter=emd_kor_nm:like:${searchTerm}`)
         .then(response => response.json())
         .then(data => {
             if (data.response && data.response.status === 'OK' && data.response.result && data.response.result.featureCollection.features) {
