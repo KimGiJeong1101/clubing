3960a1a546d5f7ca0a1d63777f434c6aa595bdfc
hykworld <151594104+hykworld@users.noreply.github.com>
2024-08-11 09:32:36 +0900

    Add files via upload

---
+++ frontend/src/setupProxy.js
@@ -0,0 +1,17 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'https://api.vworld.kr',
+      changeOrigin: true,
+      pathRewrite: {
+        '^/api': '', // /api를 빈 문자열로 변환
+      },
+      onProxyReq: (proxyReq, req, res) => {
+        console.log('Proxying request to:', proxyReq.url);
+      },
+    })
+  );
+};
