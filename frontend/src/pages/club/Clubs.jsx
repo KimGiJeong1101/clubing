cad69d94a276ff78b17a2d459f97bad0cbe6fc99
kookunmo <kummo9@naver.com>
2024-09-06 16:23:20 +0900

    무한 스크롤 + 클럽리스트 페이지 완료 + 검색까지 완료

--- frontend/src/pages/club/Clubs.jsx
+++ frontend/src/pages/club/Clubs.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Box, Button, Container, Grid, Paper, Typography } from "@mui/material";
 import CommentRoundedIcon from "@mui/icons-material/CommentRounded";
 import PeopleRoundedIcon from "@mui/icons-material/PeopleRounded";
@@ -10,7 +10,6 @@ import { useNavigate } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import BubbleAnimation from "../../components/club/BubbleAnimation.js";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
-import StorefrontIcon from "@mui/icons-material/Storefront";
 import FastfoodIcon from "@mui/icons-material/Fastfood";
 import MenuBookIcon from "@mui/icons-material/MenuBook";
 import NightlifeIcon from "@mui/icons-material/Nightlife";
@@ -23,67 +22,130 @@ import LoyaltyIcon from "@mui/icons-material/Loyalty";
 import LocalAirportIcon from "@mui/icons-material/LocalAirport";
 import Diversity1Icon from "@mui/icons-material/Diversity1";
 import AutoStoriesIcon from "@mui/icons-material/AutoStories";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import { throttle } from "lodash";
+import { textAlign } from "@mui/system";
 const Clubs = () => {
-  const getClubList = async () => {
-    const response = await fetch("http://localhost:4000/clubs");
-    const data = await response.json();
-    return data;
-  };
+  let [category, setCategory] = useState("");
+  //검색 스위치
+  const [searchRegion, setSearchRegion] = useState("");
 
+  const getClubList = async () => {
+    if (!category) {
+      const response = await fetch(`http://localhost:4000/clubs?searchRegion=${searchRegion}`);
+      const data = await response.json();
+      if (!data || data.length < 6) {
+        window.removeEventListener("scroll", handleScroll);
+      }
+      return data;
+    } else if (category) {
+      const response = await fetch(`http://localhost:4000/clubs/${category}?searchRegion=${searchRegion}`);
+      const data = await response.json();
+      if (!data || data.length < 6) {
+        window.removeEventListener("scroll", handleScroll);
+      }
+      return data;
+    }
+  };
   const {
     data: clubList,
     error,
     isLoading,
     isError,
   } = useQuery({
-    queryKey: ["clubList"],
+    queryKey: ["clubList", category, searchRegion],
     queryFn: getClubList,
   });
+  //카테고리 바뀔 떄 마다 리스트를 불러옴 -> 어차피 3개씩 불러와서 빨리빨리 부르는데 헉 생각해보니...
   const navigate = useNavigate();
-
   //무한스크롤 구현
-  const getClubListScroll = async () => {
-    const response = await fetch(`http://localhost:4000/clubs/scroll/${scrollCount}`);
+  const getClubListScroll = async (newScrollCount) => {
+    let response;
+    if (!category) {
+      response = await fetch(`http://localhost:4000/clubs/scroll/${newScrollCount}?searchRegion=${searchRegion}`);
+    } else {
+      response = await fetch(`http://localhost:4000/clubs/scroll/${newScrollCount}/${category}?searchRegion=${searchRegion}`);
+    }
     const data = await response.json();
+    setScrollData((prevData) => [...prevData, ...data]); // Merge previous and new data
 
-    setScrollData((prevData) => [...prevData, ...data]); // 이전 데이터와 새 데이터를 병합
-    if (data.length === 3) {
+    if (data.length === 6) {
       window.addEventListener("scroll", handleScroll);
-    } else if (data.length !== 3) {
+    } else {
+      window.removeEventListener("scroll", handleScroll);
     }
   };
   let [scrollCount, setScrollCount] = useState(1);
   let [scrollData, setScrollData] = useState([]);
 
-  let handleScroll = () => {
-    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    let scrollHeight = document.documentElement.scrollHeight;
-    let clientHeight = window.innerHeight;
-    if (scrollTop + clientHeight >= scrollHeight - 10) {
-      setScrollCount(scrollCount++);
-      getClubListScroll();
-      window.removeEventListener("scroll", handleScroll);
-    }
-  };
+  let handleScroll = useCallback(
+    throttle(() => {
+      let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      let scrollHeight = document.documentElement.scrollHeight;
+      let clientHeight = window.innerHeight;
+
+      if (scrollTop + clientHeight >= scrollHeight - 10) {
+        setScrollCount((prevCount) => {
+          const newCount = prevCount + 1;
+
+          getClubListScroll(newCount);
+          return newCount;
+        });
+        window.removeEventListener("scroll", handleScroll);
+      }
+    }, 500),
+    [category, scrollCount, searchRegion],
+  );
 
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
     return () => {
-      window.removeEventListener("scroll", handleScroll); // 클리어 하기위한 함수
+      window.removeEventListener("scroll", handleScroll); // 클린 업 펑션
     };
-  }, []);
+  }, [category, searchRegion]);
   //무한스크롤 구현.end
 
   //추천
   const handleCategoryClick = () => {
     // 카테고리별 버튼 클릭 시 동작
-    console.log('카테고리별 클릭');
   };
 
   const handleRegionClick = () => {
-    // 지역별 버튼 클릭 시 동작
-    console.log('지역별 클릭');
+    setScrollData([]);
+    setScrollCount(1);
+    new window.daum.Postcode({
+      oncomplete: function (data) {
+        let addr = ""; // 주소 변수
+
+        // 사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져옴
+        if (data.userSelectedType === "R") {
+          addr = data.roadAddress; // 도로명 주소
+        } else {
+          addr = data.jibunAddress; // 지번 주소
+        }
+        setSearchRegion(addr.split(" ")[1]);
+      },
+    }).open();
   };
+
+  const handleCategoryClick2 = (item) => {
+    setScrollData([]);
+    setScrollCount(1);
+    setCategory(item.text);
+  };
+
+  //지도스크립트 useEffect
+  useEffect(() => {
+    // Daum Postcode API 스크립트 로드
+    const script = document.createElement("script");
+    script.src = "//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js";
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script); // 컴포넌트 언마운트 시 스크립트 정리
+    };
+  }, []);
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -99,9 +161,10 @@ const Clubs = () => {
         onClick={() => {
           navigate("/clubs/create");
         }}
-        color="primary"
         aria-label="add"
         style={{
+          backgroundColor: "#A6836F",
+          color: "white",
           position: "fixed",
           bottom: "50px",
           right: "50px",
@@ -124,7 +187,7 @@ const Clubs = () => {
           카테고리별
         </Button>
         <Button variant="contained" color="secondary" onClick={handleRegionClick}>
-          지역별
+          지역별 검색
         </Button>
       </Box>
       <Box sx={{ width: "100%", height: "400px", backgroundColor: "white" }}>
@@ -147,6 +210,9 @@ const Clubs = () => {
             ].map((item, index) => (
               <Grid item xs={2} sm={2} lg={2} key={index}>
                 <Box
+                  onClick={() => {
+                    handleCategoryClick2(item);
+                  }}
                   sx={{
                     display: "flex",
                     flexDirection: "column",
@@ -186,6 +252,95 @@ const Clubs = () => {
         </Container>
       </Box>
       <Container maxWidth="lg" sx={{ marginTop: "40px", paddingBottom: "40px" }}>
+        <Grid container spacing={2} sx={{ display: "flex" }}>
+          {searchRegion && (
+            <div style={{ position: "relative", display: "flex", alignItems: "center", marginBottom: "10px" }}>
+              <Typography
+                variant={"h6"}
+                style={{
+                  backgroundColor: "#fff",
+                  padding: "12px 20px",
+                  borderRadius: "20px",
+                  color: "#A6836F",
+                  boxShadow: "0px 2px 10px rgba(0, 0, 0, 0.1)",
+                  position: "relative", // 부모 요소가 위치를 기준으로 버튼을 배치
+                  opacity: "0.8",
+                }}
+              >
+                <b>"{searchRegion}"</b>로 검색한 결과
+                <Box
+                  onClick={() => setSearchRegion("")}
+                  style={{
+                    position: "absolute", // 절대 위치 설정
+                    top: "-10px", // Typography 오른쪽 위에 배치
+                    right: "-10px",
+                    width: "30px ",
+                    height: "30px ",
+                    backgroundColor: "#A6836F",
+                    borderRadius: "15px", // 완전한 원 모양으로 설정
+                    color: "#ffffff",
+                    fontSize: "12px", // 작은 글씨 크기
+                    opacity: "0.9",
+                    transition: "opacity 0.3s ease",
+                    boxShadow: "0px 2px 5px rgba(0, 0, 0, 0.1)", // 버튼에 작은 그림자 추가
+                    zIndex: "1",
+                    margin: "0",
+                    padding: "0",
+                    textAlign: "center",
+                    alignItems: "center",
+                  }}
+                  onMouseOver={(e) => (e.target.style.cursor = "pointer")}
+                >
+                  <DeleteForeverIcon sx={{ paddingTop: "2px" }} />
+                </Box>
+              </Typography>
+            </div>
+          )}
+          {category && (
+            <Box sx={{ position: "relative", display: "flex", alignItems: "center", marginBottom: "10px", marginLeft: "20px" }}>
+              <Typography
+                variant={"h6"}
+                style={{
+                  backgroundColor: "#fff",
+                  padding: "12px 20px",
+                  borderRadius: "20px",
+                  color: "#A6836F",
+                  boxShadow: "0px 2px 10px rgba(0, 0, 0, 0.1)",
+                  position: "relative", // 부모 요소가 위치를 기준으로 버튼을 배치
+                  opacity: "0.8",
+                }}
+              >
+                <b>{category}</b>
+                <Box
+                  onClick={() => setCategory("")}
+                  style={{
+                    position: "absolute", // 절대 위치 설정
+                    top: "-10px", // Typography 오른쪽 위에 배치
+                    right: "-10px",
+                    width: "30px ",
+                    height: "30px ",
+                    backgroundColor: "#A6836F",
+                    borderRadius: "15px", // 완전한 원 모양으로 설정
+                    color: "#ffffff",
+                    fontSize: "12px", // 작은 글씨 크기
+                    opacity: "0.9",
+                    transition: "opacity 0.3s ease",
+                    boxShadow: "0px 2px 5px rgba(0, 0, 0, 0.1)", // 버튼에 작은 그림자 추가
+                    zIndex: "1",
+                    margin: "0",
+                    padding: "0",
+                    textAlign: "center",
+                    alignItems: "center",
+                  }}
+                  onMouseOver={(e) => (e.target.style.cursor = "pointer")}
+                >
+                  <DeleteForeverIcon sx={{ paddingTop: "2px" }} />
+                </Box>
+              </Typography>
+            </Box>
+          )}
+        </Grid>
+
         <Grid container spacing={3} sx={{ mb: 3 }}>
           {clubList.map((club) => (
             <Grid item xs={12} sm={6} md={4} key={club._id} sx={{}}>
@@ -246,7 +401,7 @@ const Clubs = () => {
                         alignItems: "center",
                         width: "130px",
                         height: "50px",
-                        color: "#3f51b5",
+                        color: "#A6836F",
                         fontWeight: "bold",
                         borderRadius: "20px",
                         backgroundColor: "white",
@@ -364,10 +519,10 @@ const Clubs = () => {
                         alignItems: "center",
                         width: "50px",
                         height: "50px",
-                        color: "#3f51b5",
+                        color: "white",
                         fontWeight: "bold",
                         borderRadius: "25px",
-                        backgroundColor: "black",
+                        backgroundColor: "#A6836F",
                         transition: "all 0.3s ease", // 모든 속성에 대해 부드럽게 변환
                         "&:hover": {
                           transform: "scale(1.2)", // 호버 시 크기 확대
