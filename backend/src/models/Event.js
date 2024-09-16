f0b7daf736d38c9b952448f413c16ab74ffaacc6
Yeon <zmlee12@naver.com>
2024-09-17 01:26:41 +0900

    Feat : Event게시판 등록 개발 및 백엔드 express 버전 업

--- backend/src/models/Event.js
+++ backend/src/models/Event.js
@@ -4,15 +4,26 @@ const { getNextSequenceValue } = require("../util/sequence");
 const eventSchema = mongoose.Schema(
   {
     _id: Number,
-    clubNumber: Number,
-    writer: String,
-    title: String,
-    category: String,
+    writer: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
     content: { type: String, default: false },
+    cardImage: {
+      type: String,
+      required: false,
+    },
+    cardTitle: {
+      type: String,
+      required: false,
+    },
   },
   { timestamps: true },
 );
-
 
 eventSchema.pre("save", async function (next) {
   if (this.isNew) {
