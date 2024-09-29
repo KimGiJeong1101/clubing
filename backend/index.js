0cea008fff6c3f2f658da6b58a30460bfc46f3a9
kkj <odgwmcsoqw@naver.com>
2024-09-29 17:49:08 +0900

    pull 하기 전 커밋

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const app = express();
 const path = require("path");
 const http = require("http");
 const socketIo = require("socket.io");
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 
 const jwt = require("jsonwebtoken"); // JWT 패키지 로드
